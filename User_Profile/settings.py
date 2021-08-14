4ed67e61ce786aac3cca070e36640d14368abf6c
SSrividhya <svidhya2512@gmail.com>
2021-08-14 18:48:47 +0000

    Added a .gitignore file

--- User_Profile/settings.py
+++ User_Profile/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n5lfp*!pp&+^m1oo*9ddeg^4aa^z@l0x&jhvr0pzrbj-!!+=4i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['srividhyas.pythonanywhere.com',]
 
 
 # Application definition
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT=BASE_DIR / 'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
